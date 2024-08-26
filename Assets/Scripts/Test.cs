064d3a5ba2fc6e21640d4a018669ffd9beec0515
PaulieMoly <darksticker@gmail.com>
2024-08-27 00:14:43 +0800

    v1.1.0 Notification and Response
    - Finished mechanics of displaying and setting notifications and responses respectively
    
    - Need to finish up all the notification and dialogues

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,58 +1,94 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Test : MonoBehaviour
 {
     [Header("Phone Settings")]
     public GameObject phoneMenu;
-    public GameObject notif1;
+    public GameObject notif;
 
     [Header("Messages")]
-    public GameObject[] responses;
+    public GameObject[] notifications;
+    [SerializeField] private List<GameObject> responses;
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        SetResponse(0);
+    }
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F) && phoneMenu.activeSelf == false)
+        // Toggle Phone UI (Frame only)
+        if (Input.GetKeyDown(KeyCode.F) && !phoneMenu.activeSelf)
         {
             phoneMenu.SetActive(true);
         }
-        else if (Input.GetKeyDown(KeyCode.F) && phoneMenu.activeSelf == true)
+        else if (Input.GetKeyDown(KeyCode.F) && phoneMenu.activeSelf)
         {
             phoneMenu.SetActive(false);
         }
 
-        if (Input.GetKeyDown(KeyCode.X) && notif1.activeSelf == false)
+        // Toggle current notification initial content
+        if (Input.GetKeyDown(KeyCode.X) && !notif.activeSelf)
         {
-            notif1.SetActive(true);
+            notif.SetActive(true);
         }
-        else if (Input.GetKeyDown(KeyCode.X) && notif1.activeSelf == true)
+        else if (Input.GetKeyDown(KeyCode.X) && notif.activeSelf)
         {
-            notif1.SetActive(false);
+            notif.SetActive(false);
         }
 
-        if (Input.GetKeyDown(KeyCode.Y) && responses[0].activeSelf == false)
+        // Display responses set for current notification [Developer Tool]
+        if (Input.GetKeyDown(KeyCode.Y) && responses.Count > 0 && !responses[0].activeSelf)
         {
             StartCoroutine(PlayResponse());
         }
-        else if (Input.GetKeyDown(KeyCode.Y) && responses[0].activeSelf == true)
+        else if (Input.GetKeyDown(KeyCode.Y) && responses.Count > 0 && responses[0].activeSelf)
         {
-            foreach(GameObject response in responses)
+            foreach (GameObject response in responses)
             {
                 response.SetActive(false);
             }
         }
+
+        // Initialize responses for 1st notification (0 in notifications array) [Developer Tool]
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            SetResponse(0);
+        }
+
+        // Initialize responses for 2nd notification (1 in notifications array) [Developer Tool]
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            SetResponse(1);
+        }
     }
 
+    // Call to display player responses
     IEnumerator PlayResponse()
     {
-        for (int i = 0; i < responses.Length; i++)
+        for (int i = 0; i < responses.Count; i++)
         {
             responses[i].SetActive(true);
-
             yield return new WaitForSeconds(1.5f);
+        }
+    }
+
+    // Call and pass notification number (in array, set in editor)
+    public void SetResponse(int notificationNumber)
+    {
+        // Reset List to 0
+        responses.Clear();
+
+        notif = notifications[notificationNumber];
+
+        foreach (Transform child in notif.transform)
+        {
+            if (child.CompareTag("Response"))
+            {
+                responses.Add(child.gameObject);
+            }
         }
     }
 }
